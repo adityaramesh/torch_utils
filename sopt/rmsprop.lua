3e2da33abc4e49e3b38bcb03cd538823da5790d7
Aditya Ramesh <_@adityaramesh.com>
2015-08-30 14:03:10 -0400

    Updated according to new interface; fixed NAG updates.

--- sopt/rmsprop.lua
+++ sopt/rmsprop.lua
@@ -1,48 +1,186 @@
-function sopt.rmsprop(func, x, config, state)
-	local config   = config or {}
-	local state    = state or config
-	local eps      = config.epsilon or 1e-10
-	local lr       = config.learning_rate or sopt.constant(1)
-	local decay    = config.decay or sopt.constant(0.95)
-	local mom      = config.momentum or sopt.sutskever_blend(0.999)
-	local mom_type = config.momentum_type or sopt.none
-	state.iter = state.iter or 0
+RMSPropOptimizer = {}
+RMSPropOptimizer.__index = RMSPropOptimizer
 
-	local k = state.iter
-	local cur_lr = lr(k)
-	local cur_decay = decay(k)
-	state.iter = state.iter + 1
+local function isnan(x)
+	return x ~= x
+end
 
-	if not state.temp then
-		state.temp = torch.Tensor():typeAs(x):resizeAs(x):zero()
-		state.exp_grad_fx = torch.Tensor():typeAs(x):resizeAs(x):zero()
+--
+-- Note: the `model` parameter here is unused, but kept anyway to preserve API
+-- uniformity. Other optimization algorithms may need to use this parameter to
+-- perform model-specific operations (e.g. disabling/enabling dropout).
+--
+function RMSPropOptimizer.create(model, params, grad_params, grad_func, state, logger)
+	local self = {}
+	setmetatable(self, RMSPropOptimizer)
+
+	self.params      = params
+	self.grad_params = grad_params
+	self.grad_func   = grad_func
+	self.state       = state or {}
+
+	self.state.iter = self.state.iter          or 0
+	self.eps        = self.state.eps           or 1e-10
+	self.lr         = self.state.learning_rate or sopt.constant(1e-3)
+	self.mom        = self.state.momentum      or sopt.constant(0.95)
+	self.decay      = self.state.decay         or sopt.constant(0.95)
+	self.mom_type   = self.state.momentum_type or sopt.none
+
+	if logger then
+		self.logger = logger
+		self.logger:add_fields({"loss", "norm_grad", "theta", "eta_a", "eta_w"})
+	end
+	return self
+end
+
+--
+-- Log function for RMSProp without NAG.
+--
+function RMSPropOptimizer:log_info(input, target, cur_lr, loss)
+	if not self.logger then return end
+
+	if not self.prev_grad_params then
+		self.prev_grad_params = torch.Tensor():typeAs(self.grad_params):
+			resizeAs(grad_params)
 	end
 
-	if mom_type == sopt.none then
-		local fx, grad_fx = func(x)
+	local norm_grad = self.grad_params:norm()
+	-- Note that descent := `1 / cur_lr * proj`. Because of cancellation
+	-- with `cur_lr` that occurs in the formulas, we don't actually define
+	-- it this way.
+	local proj = -self.state.temp:dot(self.grad_params)
+	local theta = math.acos(proj / (self.state.temp:norm() * norm_grad))
 
-		state.temp:pow(grad_fx, 2):mul(1 - cur_decay)
-		state.exp_grad_fx:mul(cur_decay):add(state.temp)
-		state.temp:add(state.exp_grad_fx, eps):sqrt()
-		x:addcdiv(-cur_lr, grad_fx, state.temp)
-		return x, {fx}
-	elseif mom_type == sopt.nag then
-		local cur_mom = mom(k)
-		cur_mom = 0
+	self.prev_grad_params:copy(self.grad_params)
+	local new_loss = self.grad_func(input, target)
+	local eta_a = (new_loss - loss) / (cur_lr * proj)
+	local eta_w = math.abs(-self.state.temp:dot(self.grad_params) / proj)
 
-		-- Evaluate the function at the test point. At this time,
-		-- `state.temp` contains the update from the previous iteration.
-		state.temp:add(x, cur_mom, state.temp)
-		local fx, grad_fx = func(state.temp)
+	self.logger:log_value("loss", loss)
+	self.logger:log_value("norm_grad", norm_grad)
+	self.logger:log_value("theta", theta)
+	self.logger:log_value("eta_a", eta_a)
+	self.logger:log_value("eta_w", eta_w)
+end
+
+--
+-- Log function for RMSProp with NAG. Currently the implementation is exactly
+-- the same as the one in `sgu.lua`.
+--
+function RMSPropOptimizer:log_nag_info(input, target, cur_lr, loss)
+	if not self.logger then return end
+	assert(self.state.prev_params ~= nil)
+	assert(self.state.prev_grad_params ~= nil)
+
+	-- See comment for analogous function in `sgu.lua` for more information
+	-- regarding the computations performed below.
+
+	local norm_grad = self.state.prev_grad_params:norm()
+	-- Note that descent := `1 / cur_lr * proj`. Because of cancellation
+	-- with `cur_lr` that occurs in the formulas, we don't actually define
+	-- it this way.
+	local proj = self.state.step:dot(self.prev_grad_params)
+	-- Note that theta could be NaN. If this happens, then either the update
+	-- or the gradient has very small magnitude, so the angle could not be
+	-- computed in single precision.
+	local theta = math.acos(proj / (self.state.step:norm() * norm_grad))
+
+	local new_loss = self.grad_func(input, target)
+	local eta_a = cur_lr * (new_loss - loss) / proj
+	local eta_w = math.abs(self.state.step:dot(self.grad_params) / proj)
+
+	self.logger:log_value("loss", loss)
+	self.logger:log_value("norm_grad", norm_grad)
+	self.logger:log_value("theta", theta)
+	self.logger:log_value("eta_a", eta_a)
+	self.logger:log_value("eta_w", eta_w)
+end
+
+function RMSPropOptimizer:update(input, target)
+	local iter = self.state.iter
+	self.state.iter = self.state.iter + 1
+
+	local cur_lr = self.lr(iter)
+	local cur_decay = self.decay(iter)
+	assert(cur_lr > 0 and cur_lr <= 1)
+	assert(cur_decay > 0 and cur_decay < 1)
+
+	if not self.state.temp then
+		-- Used as a buffer to store intermediate results.
+		self.state.temp = torch.Tensor():typeAs(self.params):
+			resizeAs(self.params):zero()
+		-- Estimate of the second moment of the gradient.
+		self.state.grad_mom_2 = torch.Tensor():typeAs(self.params):
+			resizeAs(self.params):zero()
+	end
+
+	if self.mom_type == sopt.none then
+		local loss = self.grad_func(input, target, true)
+
+		-- Update the estimate of the second moment of the gradient.
+		if not self.state.temp then
+			-- At the first iteration, we shouldn't multiply the
+			-- second moment estimate by the decay factor.
+			self.state.grad_mom_2 = self.grad_params:clone():pow(2)
+			self.state.temp = self.grad_mom_2:clone():
+				add(self.state.eps):sqrt()
+		else
+			self.state.temp:pow(self.grad_params, 2)
+			self.state.grad_mom_2:mul(cur_decay):
+				add(1 - cur_decay, self.state.temp)
+			self.state.temp:add(self.state.grad_mom_2, self.state.eps):
+				sqrt()
+		end
+
+		self.params:addcdiv(-cur_lr, self.grad_params, self.state.temp)
+		self:log_info(input, target, cur_lr, loss)
+	elseif self.mom_type == sopt.nag then
+		if not self.state.step then
+			self.state.step = torch.Tensor():typeAs(self.params):
+				resizeAs(self.params):zero()
+
+			if self.logger then
+				self.state.prev_params = self.params:clone()
+				self.state.prev_grad_params = self.grad_params:clone()
+			end
+		end
+
+		local cur_mom = self.mom(iter)
+		assert(cur_mom > 0 and cur_mom < 1)
+
+		-- Evaluate the function at the trial point.
+		self.state.step:mul(cur_mom)
+		self.params:add(self.state.step)
+		local loss = self.grad_func(input, target, true)
+
+		-- Update the estimate of the second moment of the gradient.
+		if not self.state.temp then
+			-- At the first iteration, we shouldn't multiply the
+			-- second moment estimate by the decay factor.
+			self.state.grad_mom_2 = self.grad_params:clone():pow(2)
+			self.state.temp = self.grad_mom_2:clone():
+				add(self.state.eps):sqrt()
+		else
+			self.state.temp:pow(self.grad_params, 2)
+			self.state.grad_mom_2:mul(cur_decay):
+				add(1 - cur_decay, self.state.temp)
+			self.state.temp:add(self.state.grad_mom_2, self.state.eps):
+				sqrt()
+		end
+
+		self.state.temp:cdiv(self.grad_params, self.state.temp):mul(-cur_lr)
 
 		-- Update the parameters.
-		state.temp:pow(grad_fx, 2):mul(1 - cur_decay)
-		state.exp_grad_fx:mul(cur_decay):add(state.temp)
-		state.temp:add(state.exp_grad_fx, eps):sqrt()
-		state.temp:cdiv(grad_fx, state.temp):mul(-cur_lr)
-		x:add(state.temp)
-		return x, {fx}
+		self.state.step:add(self.state.temp)
+		self.params:add(self.state.temp)
+
+		if self.logger then
+			self:log_nag_info(input, target, cur_lr, loss)
+			self.state.prev_params:copy(self.params)
+			self.grad_func(input, target)
+			self.state.prev_grad_params:copy(self.grad_params)
+		end
 	else
-		error("Invalid momentum type '" .. mom_type .. "'.")
+		error("Unsupported momentum type.")
 	end
 end
--- sopt/sgu.lua
+++ sopt/sgu.lua
@@ -34,7 +34,7 @@ end
 --
 -- Log function for SGU without NAG.
 --
-function SGUOptimizer:log_sgu_info(input, target, cur_lr, loss)
+function SGUOptimizer:log_info(input, target, cur_lr, loss)
 	if not self.logger then return end
 
 	if not self.prev_grad_params then
@@ -49,8 +49,8 @@ function SGUOptimizer:log_sgu_info(input, target, cur_lr, loss)
 	local new_loss = self.grad_func(input, target)
 
 	local eta_a = (new_loss -  loss) / (cur_lr * descent)
-	local eta_w = math.abs(self.prev_grad_params:dot(
-		self.grad_params) / descent)
+	local eta_w = math.abs(-self.grad_params:dot(
+		self.prev_grad_params) / descent)
 
 	self.logger:log_value("loss", loss)
 	self.logger:log_value("norm_grad", norm_grad)
@@ -62,7 +62,11 @@ end
 --
 -- Log function for SGU with NAG.
 --
-function SGUOptimizer:log_sgu_nag_info(input, target, cur_lr, loss)
+-- Precondition: `self.grad_params` is *not* modified.
+-- Precondition: `self.state.step` =: s_k is defined such that
+-- `x_{k + 1} = x_k + s_k`.
+--
+function SGUOptimizer:log_nag_info(input, target, cur_lr, loss)
 	if not self.logger then return end
 	assert(self.state.prev_params ~= nil)
 	assert(self.state.prev_grad_params ~= nil)
@@ -76,20 +80,19 @@ function SGUOptimizer:log_sgu_nag_info(input, target, cur_lr, loss)
 	-- p_{k + 1} = (1 / eta) * s_{k + 1}. Thus we use p_{k + 1} instead of
 	-- s_{k + 1} to compute the quantities below.
 
-	local norm_grad = self.prev_grad_params:norm()
+	local norm_grad = self.state.prev_grad_params:norm()
 	-- Note that descent := `1 / cur_lr * proj`. Because of cancellation
 	-- with `cur_lr` that occurs in the formulas, we don't actually define
 	-- it this way.
-	local proj = self.state.temp:dot(self.prev_grad_params)
+	local proj = self.state.step:dot(self.prev_grad_params)
 	-- Note that theta could be NaN. If this happens, then either the update
 	-- or the gradient has very small magnitude, so the angle could not be
 	-- computed in single precision.
-	local theta = math.acos(proj / self.state.temp:norm() *
-		self.prev_grad_params:norm())
+	local theta = math.acos(proj / (self.state.step:norm() * norm_grad))
 
 	local new_loss = self.grad_func(input, target)
 	local eta_a = cur_lr * (new_loss - loss) / proj
-	local eta_w = math.abs(self.state.temp:dot(self.grad_params) / proj)
+	local eta_w = math.abs(self.state.step:dot(self.grad_params) / proj)
 
 	self.logger:log_value("loss", loss)
 	self.logger:log_value("norm_grad", norm_grad)
@@ -103,19 +106,19 @@ function SGUOptimizer:update(input, target)
 	self.state.iter = self.state.iter + 1
 
 	local cur_lr = self.lr(iter)
-	assert(cur_lr > 0)
+	assert(cur_lr > 0 and cur_lr <= 1)
 
 	if self.mom_type == sopt.none then
 		local loss = self.grad_func(input, target, true)
 		self.params:add(-cur_lr, self.grad_params)
-		self:log_sgu_info(input, target, cur_lr, loss)
+		self:log_info(input, target, cur_lr, loss)
 	elseif self.mom_type == sopt.nag then
 		-- For the first iteration, we just take the direction of
 		-- steepest descent.
-		if not self.state.temp then
+		if not self.state.step then
 			local loss = self.grad_func(input, target, true)
-			self.state.temp = self.grad_params:clone():mul(-cur_lr)
-			self.params:add(self.state.temp)
+			self.state.step = self.grad_params:clone():mul(-cur_lr)
+			self.params:add(self.state.step)
 
 			if self.logger then
 				-- Unlike vanilla SGU, `prev_params` and
@@ -125,7 +128,7 @@ function SGUOptimizer:update(input, target)
 				-- temporary buffers.
 				self.state.prev_params = self.params:clone()
 				self.state.prev_grad_params = self.grad_params:clone()
-				self:log_sgu_nag_info(input, target, cur_lr, loss)
+				self:log_nag_info(input, target, cur_lr, loss)
 			end
 			return
 		end
@@ -134,16 +137,18 @@ function SGUOptimizer:update(input, target)
 		assert(cur_mom > 0 and cur_mom < 1)
 
 		-- Evaluate the function at the trial point.
-		self.state.temp:mul(cur_mom)
-		self.params:add(self.state.temp)
+		self.state.step:mul(cur_mom)
+		self.params:add(self.state.step)
 		local loss = self.grad_func(input, target, true)
 
-		---- Update the parameters.
-		self.state.temp:add(-cur_lr, self.grad_params)
+		-- Update the parameters. We don't multiply the gradient by
+		-- `-cur_lr` in advance because the logging function requires
+		-- the original value.
+		self.state.step:add(-cur_lr, self.grad_params)
 		self.params:add(-cur_lr, self.grad_params)
 
 		if self.logger then
-			self:log_sgu_nag_info(input, target, cur_lr, loss)
+			self:log_nag_info(input, target, cur_lr, loss)
 			self.state.prev_params:copy(self.params)
 			self.grad_func(input, target)
 			self.state.prev_grad_params:copy(self.grad_params)
