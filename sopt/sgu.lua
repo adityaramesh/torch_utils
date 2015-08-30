d538f03328578eae415b3f02f04d024f2073efe5
Aditya Ramesh <_@adityaramesh.com>
2015-08-30 05:52:46 -0400

    Fixed computations for SGU NAG logger.

--- sopt/sgu.lua
+++ sopt/sgu.lua
@@ -10,14 +10,14 @@ end
 -- uniformity. Other optimization algorithms may need to use this parameter to
 -- perform model-specific operations (e.g. disabling/enabling dropout).
 --
-function SGUOptimizer.create(model, params, grad_params, grad_func, config, logger)
+function SGUOptimizer.create(model, params, grad_params, grad_func, state, logger)
 	local self = {}
 	setmetatable(self, SGUOptimizer)
 
 	self.params      = params
 	self.grad_params = grad_params
 	self.grad_func   = grad_func
-	self.state       = config or {}
+	self.state       = state or {}
 
 	self.state.iter = self.state.iter          or 0
 	self.lr         = self.state.learning_rate or sopt.constant(1e-3)
@@ -27,61 +27,75 @@ function SGUOptimizer.create(model, params, grad_params, grad_func, config, logg
 	if logger then
 		self.logger = logger
 		self.logger:add_fields({"loss", "norm_grad", "theta", "eta_a", "eta_w"})
-		self.prev_grad_params = torch.Tensor():typeAs(grad_params):
-			resizeAs(grad_params)
 	end
 	return self
 end
 
+--
+-- Log function for SGU without NAG.
+--
 function SGUOptimizer:log_sgu_info(input, target, cur_lr, loss)
-	if self.logger then
-		local norm_grad = self.grad_params:norm()
-		local descent = -math.pow(norm_grad, 2)
-		self.prev_grad_params:copy(self.grad_params)
-		local new_loss = self.grad_func(input, target)
+	if not self.logger then return end
 
-		local eta_a = (new_loss -  loss) / (cur_lr * descent)
-		local eta_w = math.abs(self.prev_grad_params:dot(
-			self.grad_params) / descent)
-
-		self.logger:log_value("loss", loss)
-		self.logger:log_value("norm_grad", norm_grad)
-		self.logger:log_value("theta", math.pi)
-		self.logger:log_value("eta_a", eta_a)
-		self.logger:log_value("eta_w", eta_w)
+	if not self.prev_grad_params then
+		self.prev_grad_params = torch.Tensor():typeAs(self.grad_params):
+			resizeAs(grad_params)
 	end
+
+	local norm_grad = self.grad_params:norm()
+	-- In our case, descent := p_k g_k = -||g_k||^2.
+	local descent = -math.pow(norm_grad, 2)
+	self.prev_grad_params:copy(self.grad_params)
+	local new_loss = self.grad_func(input, target)
+
+	local eta_a = (new_loss -  loss) / (cur_lr * descent)
+	local eta_w = math.abs(self.prev_grad_params:dot(
+		self.grad_params) / descent)
+
+	self.logger:log_value("loss", loss)
+	self.logger:log_value("norm_grad", norm_grad)
+	self.logger:log_value("theta", math.pi)
+	self.logger:log_value("eta_a", eta_a)
+	self.logger:log_value("eta_w", eta_w)
 end
 
+--
+-- Log function for SGU with NAG.
+--
 function SGUOptimizer:log_sgu_nag_info(input, target, cur_lr, loss)
-	if self.logger then
-		-- In order to allow for a more direct comparison to SGD, we
-		-- make the following observation regarding the NAG update:
-		-- 	s_{k + 1} := mu * s_k - eta * hat{g}_{k + 1}
-		-- 	           = eta(mu / eta * s_k - hat{g}_{k + 1})
-		-- 	          := eta * p_{k + 1}.
-		-- Thus p_{k + 1} = (1 / eta) * s_{k + 1}. So we use p_{k + 1}
-		-- instead of s_{k + 1} to compute the quantities that we log.
+	if not self.logger then return end
+	assert(self.state.prev_params ~= nil)
+	assert(self.state.prev_grad_params ~= nil)
 
-		local norm_grad = self.grad_params:norm()
-		local descent = 1 / cur_lr * self.state.temp:dot(self.grad_params)
-		local theta = math.acos(descent / (1 / (cur_lr * cur_lr) *
-			self.state.temp:norm() * norm_grad))
+	-- In order to allow for a more direct comparison to SGD, we
+	-- make the following observation regarding the NAG update:
+	-- 	s_{k + 1} :=  mu * s_k - eta * hat{g}_{k + 1}
+	-- 	           =  eta(mu / eta * s_k - hat{g}_{k + 1})
+	-- 	           =: eta * p_{k + 1}.
+	-- So the analogous notion of "search direction" for SGU with NAG is
+	-- p_{k + 1} = (1 / eta) * s_{k + 1}. Thus we use p_{k + 1} instead of
+	-- s_{k + 1} to compute the quantities below.
 
-		-- If this happens, then either the update or the gradient has
-		-- a very small magnitude. We report -1 to indicate that the
-		-- angle could not be computed.
-		if isnan(theta) then theta = -1 end
+	local norm_grad = self.prev_grad_params:norm()
+	-- Note that descent := `1 / cur_lr * proj`. Because of cancellation
+	-- with `cur_lr` that occurs in the formulas, we don't actually define
+	-- it this way.
+	local proj = self.state.temp:dot(self.prev_grad_params)
+	-- Note that theta could be NaN. If this happens, then either the update
+	-- or the gradient has very small magnitude, so the angle could not be
+	-- computed in single precision.
+	local theta = math.acos(proj / self.state.temp:norm() *
+		self.prev_grad_params:norm())
 
-		local new_loss = self.grad_func(input, target)
-		local eta_a = (new_loss - loss) / descent
-		local eta_w = math.abs(self.state.temp:dot(self.grad_params) / descent)
+	local new_loss = self.grad_func(input, target)
+	local eta_a = cur_lr * (new_loss - loss) / proj
+	local eta_w = math.abs(self.state.temp:dot(self.grad_params) / proj)
 
-		self.logger:log_value("loss", loss)
-		self.logger:log_value("norm_grad", norm_grad)
-		self.logger:log_value("theta", theta)
-		self.logger:log_value("eta_a", eta_a)
-		self.logger:log_value("eta_w", eta_w)
-	end
+	self.logger:log_value("loss", loss)
+	self.logger:log_value("norm_grad", norm_grad)
+	self.logger:log_value("theta", theta)
+	self.logger:log_value("eta_a", eta_a)
+	self.logger:log_value("eta_w", eta_w)
 end
 
 function SGUOptimizer:update(input, target)
@@ -96,11 +110,23 @@ function SGUOptimizer:update(input, target)
 		self.params:add(-cur_lr, self.grad_params)
 		self:log_sgu_info(input, target, cur_lr, loss)
 	elseif self.mom_type == sopt.nag then
+		-- For the first iteration, we just take the direction of
+		-- steepest descent.
 		if not self.state.temp then
 			local loss = self.grad_func(input, target, true)
 			self.state.temp = self.grad_params:clone():mul(-cur_lr)
 			self.params:add(self.state.temp)
-			self:log_sgu_info(input, target, cur_lr, loss)
+
+			if self.logger then
+				-- Unlike vanilla SGU, `prev_params` and
+				-- `grad_params` need to be part of the state,
+				-- since the logging function depends on their
+				-- values and does not just use them as
+				-- temporary buffers.
+				self.state.prev_params = self.params:clone()
+				self.state.prev_grad_params = self.grad_params:clone()
+				self:log_sgu_nag_info(input, target, cur_lr, loss)
+			end
 			return
 		end
 
@@ -115,7 +141,13 @@ function SGUOptimizer:update(input, target)
 		---- Update the parameters.
 		self.state.temp:add(-cur_lr, self.grad_params)
 		self.params:add(-cur_lr, self.grad_params)
-		self:log_sgu_nag_info(input, target, cur_lr, loss)
+
+		if self.logger then
+			self:log_sgu_nag_info(input, target, cur_lr, loss)
+			self.state.prev_params:copy(self.params)
+			self.grad_func(input, target)
+			self.state.prev_grad_params:copy(self.grad_params)
+		end
 	else
 		error("Unsupported momentum type.")
 	end
