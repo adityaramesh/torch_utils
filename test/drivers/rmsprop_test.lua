7ece634ab9e1013c00e8519f45e28d2d9a438d55
Aditya Ramesh <_@adityaramesh.com>
2015-08-30 20:48:44 -0400

    Added tests for sgu, rmsprop.

---
+++ test/drivers/rmsprop_test.lua
@@ -0,0 +1,17 @@
+package.path = package.path .. ';source'
+require "torch_utils/sopt"
+require "test/models/cnn_5x5"
+require "test/utility/run_model"
+
+function get_train_info()
+	return {
+		opt_state = {
+			learning_rate = sopt.constant(0.001),
+			momentum_type = sopt.none
+		},
+		opt_method = RMSPropOptimizer,
+		batch_size = 100
+	}
+end
+
+run(get_model_info, get_train_info)
--- test/utility/run_model.lua
+++ test/utility/run_model.lua
@@ -1,7 +1,6 @@
 require "torch"
 require "optim"
 require "xlua"
-
 require "torch_utils/utility"
 
 local function do_train_epoch(data, context, paths, info)
@@ -22,8 +21,8 @@ local function do_train_epoch(data, context, paths, info)
 
 	local perm = torch.randperm(train_size)
 	model:training()
-
 	print("Starting training epoch " .. info.train.epoch .. ".")
+
 	for i = 1, train_size, batch_size do
 		xlua.progress(i, train_size)
 
@@ -38,30 +37,13 @@ local function do_train_epoch(data, context, paths, info)
 			table.insert(targets, target)
 		end
 
-		inputs = nn.JoinTable(1):forward(inputs):cuda()
-		targets = nn.JoinTable(1):forward(targets):cuda()
+		input = nn.JoinTable(1):forward(inputs):typeAs(params)
+		target = nn.JoinTable(1):forward(targets):typeAs(params)
+		context.optimizer:update(input, target)
 
-		-- Create function to obtain the model's output and gradient
-		-- with respect to parameters.
-		local f = function(x)
-			if x ~= params then
-				params:copy(x)
-			end
-			grad_params:zero()
-
-			local outputs = model:forward(inputs)
-			local loss = criterion:forward(outputs, targets)
-			model:backward(inputs, criterion:backward(outputs, targets))
-			confusion:batchAdd(outputs, targets)
-			return loss, grad_params
-		end
-
-		opt_method(f, params, opt_state)
-
-		if i % 33 == 0 then
-			-- This helps prevent out of memory errors on the GPU.
-			collectgarbage()
-		end
+		context.logger:log_value("train_epoch", info.train.epoch)
+		context.logger:log_value("train_iter", (i - 1) / batch_size + 1)
+		context.logger:flush()
 	end
 
 	xlua.progress(train_size, train_size)
@@ -70,7 +52,7 @@ local function do_train_epoch(data, context, paths, info)
 	print("Mean class accuracy (training): " .. 100 * acc .. "%.")
 	confusion:zero()
 
-	model_utils.save_train_progress(function(x, y) return x > y end,
+	model_io.save_train_progress(function(x, y) return x > y end,
 		info.train.epoch, acc, paths, info)
 	info.train.epoch = info.train.epoch + 1
 end
@@ -80,6 +62,7 @@ local function do_valid_epoch(data, context, paths, info)
 	local criterion  = info.model.criterion
 	local valid_size = data.inputs:size(1)
 	local batch_size = info.train.batch_size
+	local params     = context.params
 	local confusion  = context.confusion
 	model:evaluate()
 
@@ -98,15 +81,10 @@ local function do_valid_epoch(data, context, paths, info)
 			table.insert(targets, target)
 		end
 
-		inputs = nn.JoinTable(1):forward(inputs):cuda()
-		targets = nn.JoinTable(1):forward(targets):cuda()
+		inputs = nn.JoinTable(1):forward(inputs):typeAs(params)
+		targets = nn.JoinTable(1):forward(targets):typeAs(params)
 		local outputs = model:forward(inputs)
 		confusion:batchAdd(outputs, targets)
-
-		if i % 33 == 0 then
-			-- This helps prevent out of memory errors on the GPU.
-			collectgarbage()
-		end
 	end
 
 	xlua.progress(valid_size, valid_size)
@@ -116,9 +94,40 @@ local function do_valid_epoch(data, context, paths, info)
 	confusion:zero()
 
 	if info.train.epoch ~= nil then
-		model_utils.save_test_progress(function(x, y) return x > y end,
+		model_io.save_test_progress(function(x, y) return x > y end,
 			info.train.epoch - 1, acc, paths, info)
 	end
+end
+
+function make_context(info)
+	local context = {}
+	local log_dir = paths.concat("models", info.options.model)
+	local log_path = paths.concat(log_dir, info.options.model .. "_output.log")
+	context.logger = CSVLogger.create(log_path, {"train_epoch", "train_iter"})
+		
+	local model = info.model.model
+	local criterion = info.model.criterion
+	context.params, context.grad_params = model:getParameters()
+	context.confusion = optim.ConfusionMatrix(10)
+
+	context.grad_func = function(input, target, update_confusion)
+		context.grad_params:zero()
+		local output = model:forward(input)
+		local loss = criterion:forward(output, target)
+
+		if update_confusion then
+			context.confusion:batchAdd(output, target)
+		end
+
+		model:backward(input, criterion:backward(output, target))
+		return loss
+	end
+
+	context.optimizer = info.train.opt_method.create(
+		model, context.params, context.grad_params, context.grad_func,
+		info.train.opt_state, context.logger)
+	context.logger:write_header()
+	return context
 end
 
 function run(model_info_func)
@@ -126,23 +135,20 @@ function run(model_info_func)
 	local data_dir = "data/preprocessed/"
 	local train_data = torch.load(data_dir .. "train_small.t7")
 	local valid_data = torch.load(data_dir .. "test.t7")
+	local do_train, _, paths, info = model_io.restore(
+		model_info_func, get_train_info)
 
-	local do_train, _, paths, info = model_utils.restore(
-		model_info_func, get_train_info, optimization_options)
-
-	print("Configuration options:")
-	print(info.options)
-
+	-- Since the training epoch count is only incremented after
+	-- serialization, the actual training epoch will always be one greater
+	-- than the number that has been serialized.
 	if info.train.epoch ~= nil then
 		info.train.epoch = info.train.epoch + 1
 	end
 
-	local context = {}
+	local context = make_context(info)
 	local max_epochs = info.options.max_epochs or 1000
-	context.params, context.grad_params = info.model.model:getParameters()
-	context.confusion = optim.ConfusionMatrix(10)
-
 	print("")
+
 	if do_train then
 		while info.train.epoch == nil or info.train.epoch <= max_epochs do
 			do_train_epoch(train_data, context, paths, info)
