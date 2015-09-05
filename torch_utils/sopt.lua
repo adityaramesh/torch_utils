9df1eb5063496057dbed8ce8f9e1fb613a2f1d88
Aditya Ramesh <_@adityaramesh.com>
2015-09-04 22:12:17 -0400

    Updated sopt header.

--- torch_utils/sopt.lua
+++ torch_utils/sopt.lua
@@ -5,5 +5,6 @@ require 'torch_utils/sopt/schedule'
 require 'torch_utils/sopt/sgu'
 require 'torch_utils/sopt/rmsprop'
 require 'torch_utils/sopt/adadelta'
+require 'torch_utils/sopt/adadelta_lm'
 
 return sopt
