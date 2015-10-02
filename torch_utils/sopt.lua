a3e31935ff048afb5d3fa05c1facc11393e04ee7
Aditya Ramesh <adityaramesh@users.noreply.github.com>
2015-10-02 08:32:03 -0400

    Removed unused table.

--- torch_utils/sopt.lua
+++ torch_utils/sopt.lua
@@ -1,10 +1,6 @@
-sopt = {}
-
-require 'torch_utils/sopt/momentum'
-require 'torch_utils/sopt/schedule'
-require 'torch_utils/sopt/sgu'
-require 'torch_utils/sopt/rmsprop'
-require 'torch_utils/sopt/adadelta'
-require 'torch_utils/sopt/adadelta_lm'
-
-return sopt
+require "torch_utils/sopt/momentum"
+require "torch_utils/sopt/schedule"
+require "torch_utils/sopt/sgu"
+require "torch_utils/sopt/rmsprop"
+require "torch_utils/sopt/adadelta"
+require "torch_utils/sopt/adadelta_lm"
