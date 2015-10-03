89d6df2c3e2edffb736531fff0bab0185203db7a
Aditya Ramesh <_@adityaramesh.com>
2015-10-03 09:16:12 -0400

    Added required table.

--- torch_utils/sopt.lua
+++ torch_utils/sopt.lua
@@ -1,6 +1,10 @@
+sopt = {}
+
 require "torch_utils/sopt/momentum"
 require "torch_utils/sopt/schedule"
 require "torch_utils/sopt/sgu"
 require "torch_utils/sopt/rmsprop"
 require "torch_utils/sopt/adadelta"
 require "torch_utils/sopt/adadelta_lm"
+
+return sopt
